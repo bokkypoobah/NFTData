949cd1a93f7669a4c7f5487b08f1b64c8db3f364
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2024-11-01 12:11:41 +1100

    wip

--- scripts/02_reportIt.js
+++ scripts/02_reportIt.js
@@ -5,7 +5,18 @@ const fetch = require('node-fetch');
 const { TOKENS } = require('./tokens');
 
 async function doit() {
-  console.log("TOKENS: " + JSON.stringify(TOKENS, null, 2));
+  const tokens = [];
+  // console.log("TOKENS: " + JSON.stringify(TOKENS, null, 2));
+  for (const [tokenId, d1] of Object.entries(TOKENS)) {
+    // console.log(tokenId + " => " + JSON.stringify(d1));
+    tokens.push({ tokenId, ...d1 });
+  }
+
+  tokens.sort((a, b) => {
+    return parseInt(b.tokenId) - parseInt(a.tokenId);
+  });
+
+  console.log("tokens: " + JSON.stringify(tokens, null, 2));
 }
 
 doit();
