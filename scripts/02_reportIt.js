a94547b5ca1bac5f835226580542fdc5e6499b48
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2024-11-01 12:25:47 +1100

    wip

--- scripts/02_reportIt.js
+++ scripts/02_reportIt.js
@@ -6,17 +6,36 @@ const { TOKENS } = require('./tokens');
 
 async function doit() {
   const tokens = [];
-  // console.log("TOKENS: " + JSON.stringify(TOKENS, null, 2));
   for (const [tokenId, d1] of Object.entries(TOKENS)) {
-    // console.log(tokenId + " => " + JSON.stringify(d1));
-    tokens.push({ tokenId, ...d1 });
+    let attributeCount = 0;
+    for (const a of d1.attributes) {
+      if (a.value != "None") {
+        attributeCount++;
+      }
+    }
+    tokens.push({ tokenId, attributeCount, ...d1 });
+  }
+  tokens.sort((a, b) => {
+    if (a.attributeCount == b.attributeCount) {
+      return b.tokenId - a.tokenId;
+    } else {
+      return b.attributeCount - a.attributeCount;
+    }
+  });
+  // console.log("top 50 by high attribute counts: " + JSON.stringify(tokens.slice(0, 50), null, 2));
+  let row = 0;
+  for (const token of tokens.slice(0, 100)) {
+    console.log(++row + "\t" + token.attributeCount + "\t" + token.tokenId + "\t" + token.owner + "\t" + token.attributes.map(e => e.key.toString() + ':' + e.value.toString()).join("\t"));
   }
 
-  tokens.sort((a, b) => {
-    return parseInt(b.tokenId) - parseInt(a.tokenId);
-  });
-
-  console.log("tokens: " + JSON.stringify(tokens, null, 2));
+  // tokens.sort((a, b) => {
+  //   if (a.attributeCount == b.attributeCount) {
+  //     return b.tokenId - a.tokenId;
+  //   } else {
+  //     return a.attributeCount - b.attributeCount;
+  //   }
+  // });
+  // console.log("top 50 by low attribute counts: " + JSON.stringify(tokens.slice(0, 50), null, 2));
 }
 
 doit();
