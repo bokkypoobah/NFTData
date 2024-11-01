9edccb42a2e643f670b2c8d79a239279272a60c7
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2024-11-01 15:34:29 +1100

    CryptoDickButts on sale sorted by trait count desc

--- scripts/01_getIt.js
+++ scripts/01_getIt.js
@@ -21,8 +21,10 @@ async function doit() {
   do {
     let url = "https://api.reservoir.tools/tokens/v7?collection=" + NFTADDRESS ;
     url = url + (continuation != null ? "&continuation=" + continuation : '');
-    url = url + "&includeAttributes=true&sortBy=updatedAt&limit=1000";
+    url = url + "&includeAttributes=true&sortBy=updatedAt&includeTopBid=true&includeLastSale=true&limit=500";
+    // url = url + "&sortBy=updatedAt&includeTopBid=true&includeLastSale=true&limit=1000";
     console.log(url);
+    // TODO: Add sleep
     try {
       await downloadFile(url, filename);
     } catch (e) {
@@ -32,22 +34,62 @@ async function doit() {
     try {
       var data = JSON.parse(fs.readFileSync(filename, "utf8"));
       continuation = data && data.continuation;
-      for (const token of (data && data.tokens || [])) {
-        // console.log(JSON.stringify(token, null, 2));
-        const tokenId = token.token.tokenId;
-        const name = token.token.name;
-        const owner = token.token.owner;
-        const image = token.token.image;
-        console.log("tokenId: " + tokenId + ", name: " + name + ", owner: " + owner);
+      for (const tokenData of (data && data.tokens || [])) {
+        const token = tokenData.token;
+        // console.log("token: " + JSON.stringify(token, null, 2));
+        const tokenId = token.tokenId;
+        const name = token.name;
+        const owner = token.owner;
+        const image = token.image;
+        // console.log("tokenId: " + tokenId + ", name: " + name + ", owner: " + owner);
         const attributes = [];
-        for (const attribute of token.token.attributes) {
+        for (const attribute of (token.attributes || [])) {
           attributes.push({ key: attribute.key, value: attribute.value });
         }
-        console.log("  attributes: " + JSON.stringify(attributes, null, 2));
+        // console.log("  attributes: " + JSON.stringify(attributes, null, 2));
+
+        const lastSaleTimestamp = token.lastSale && token.lastSale.timestamp || null;
+        const lastSaleCurrency = token.lastSale && token.lastSale.price && token.lastSale.price.currency && token.lastSale.price.currency.symbol || null;
+        const lastSaleAmount = token.lastSale && token.lastSale.price && token.lastSale.price.amount && token.lastSale.price.amount.native || null;
+        const lastSaleAmountUSD = token.lastSale && token.lastSale.price && token.lastSale.price.amount && token.lastSale.price.amount.usd || null;
+        const lastSale = {
+          timestamp: lastSaleAmount && lastSaleTimestamp || null,
+          currency: lastSaleAmount && lastSaleCurrency || null,
+          amount: lastSaleAmount || null,
+          amountUSD: lastSaleAmount && lastSaleAmountUSD || null,
+        };
+        const market = tokenData.market;
+        const priceSource = market.floorAsk && market.floorAsk.source && market.floorAsk.source.domain || null;
+        const priceExpiry = market.floorAsk && market.floorAsk.validUntil && parseInt(market.floorAsk.validUntil) || null;
+        const priceCurrency = market.floorAsk && market.floorAsk.price && market.floorAsk.price.currency && market.floorAsk.price.currency.symbol || null;
+        const priceAmount = market.floorAsk && market.floorAsk.price && market.floorAsk.price.amount && market.floorAsk.price.amount.native || null;
+        const priceAmountUSD = market.floorAsk && market.floorAsk.price && market.floorAsk.price.amount && market.floorAsk.price.amount.usd || null;
+        const price = {
+          source: priceAmount && priceSource || null,
+          expiry: priceAmount && priceExpiry || null,
+          currency: priceAmount && priceCurrency || null,
+          amount: priceAmount || null,
+          amountUSD: priceAmount && priceAmountUSD || null,
+        };
+        const topBidCurrency = market.topBid && market.topBid.price && market.topBid.price.currency && market.topBid.price.currency.symbol || null;
+        const topBidAmount = market.topBid && market.topBid.price && market.topBid.price.amount && market.topBid.price.amount.native || null;
+        const topBidAmountUSD = market.topBid && market.topBid.price && market.topBid.price.amount && market.topBid.price.amount.usd || null;
+        const topBidNetAmount = market.topBid && market.topBid.price && market.topBid.price.netAmount && market.topBid.price.netAmount.native || null;
+        const topBidNetAmountUSD = market.topBid && market.topBid.price && market.topBid.price.netAmount && market.topBid.price.netAmount.usd || null;
+        const topBid = {
+          currency: topBidAmount && topBidCurrency || null,
+          amount: topBidAmount || null,
+          amountUSD: topBidAmount && topBidAmountUSD || null,
+          netAmount: topBidAmount && topBidNetAmount || null,
+          netAmountUSD: topBidAmount && topBidNetAmountUSD || null,
+        };
         tokens[tokenId] = {
           name,
           owner,
           image,
+          topBid,
+          price,
+          lastSale,
           attributes,
         }
       }
--- scripts/02_reportIt.js
+++ scripts/02_reportIt.js
@@ -24,12 +24,18 @@ async function doit() {
   });
 
   // console.log("top 50 by high attribute counts: " + JSON.stringify(tokens.slice(0, 50), null, 2));
-  let row = 0;
+  let row = 1;
   let output = "<html><head><title>Report</title></head><body>";
-  output = output + "<table><tr><th>#</th><th>Attribute Count</th><th>Token Id</th><th>Image</th><th>Attributes</th></tr>";
-  for (const token of tokens.slice(0, 100)) {
+  output = output + "<table><tr><th>#</th><th>Attribute Count</th><th>Token Id</th><th>Bid</th><th>Ask</th><th>Last</th><th>Image</th><th>Attributes</th></tr>";
+  for (const token of tokens) {
     // console.log(++row + "\t" + token.attributeCount + "\t" + token.tokenId + "\t" + token.owner + "\t" + token.attributes.map(e => e.key.toString() + ':' + e.value.toString()).join("\t"));
-    output = output + "<tr><td>" + (++row) + "</td><td>" + token.attributeCount + "</td><td><a href='https://blur.io/eth/asset/0x42069abfe407c60cf4ae4112bedead391dba1cdb/" + token.tokenId + "' target='_blank'>" + token.tokenId + "</a></td><td><img src='" + token.image + "' width='100' /></td><td>" + token.attributes.map(e => e.key.toString() + ':' + e.value.toString()).join(", ") + "</td></tr>";
+    console.log(row + "\t" + JSON.stringify(token.price));
+    output = output + "<tr><td>" + row + "</td><td>" + token.attributeCount + "</td><td><a href='https://blur.io/eth/asset/0x42069abfe407c60cf4ae4112bedead391dba1cdb/" + token.tokenId + "' target='_blank'>" + token.tokenId + "</a></td>" +
+      "<td>" + (token.topBid && token.topBid.amount || '') + "</td>" +
+      "<td>" + (token.price && token.price.amount || '') + "</td>" +
+      "<td>" + (token.lastSale && token.lastSale.amount || '') + "</td>" +
+      "<td><img src='" + token.image + "' width='100' /></td><td>" + token.attributes.map(e => e.key.toString() + ':' + e.value.toString()).join(", ") + "</td></tr>";
+      row++;
   }
   output = output + "</table></body></html>";
   // console.log(output);
